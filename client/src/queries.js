a6eb3e766e63fe930d403fc2fc9e8d9b41283ca1
Palmidia Jetzabel Garay Dávila <gjetzable@gmail.com>
2018-10-24 02:16:28 -0500

    Adding form and validation

---
+++ client/src/queries.js
@@ -0,0 +1,29 @@
+import { gql } from "apollo-boost";
+
+/* User Queries */
+export const GET_CURRENT_USER = gql`
+  query {
+    getCurrentUser {
+      _id
+      username
+      email
+      password
+      avatar
+      joinDate
+      favorites {
+        _id
+        title
+        imageUrl
+      }
+    }
+  }
+`;
+
+/* User Mutations */
+export const SIGNIN_USER = gql`
+  mutation($username: String!, $password: String!) {
+    signinUser(username: $username, password: $password) {
+      username
+    }
+  }
+`;
--- client/src/store.js
+++ client/src/store.js
@@ -2,10 +2,82 @@ import Vue from "vue";
 import Vuex from "vuex";
 import { defaultClient as ApolloClient } from "./main";
 
+import { SIGNIN_USER } from "./queries";
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    user: null,
+    loading: false,
+    error: null,
+    authError: null
+  },
+  mutations: {
+    setLoading: (state, payload) => {
+      state.loading = payload;
+    },
+    setError: (state, payload) => {
+      state.error = payload;
+    },
+    setAuthError: (state, payload) => {
+      state.authError = payload;
+    },
+    clearUser: state => (state.user = null)
+  },
+  actions: {
+    getCurrentUser: ({ commit }) => {
+      commit("setLoading", true);
+      apolloClient
+        .query({
+          query: GET_CURRENT_USER
+        })
+        .then(({ data }) => {
+          commit("setLoading", false);
+          // Add user data to state
+          commit("setUser", data.getCurrentUser);
+          console.log(data.getCurrentUser);
+        })
+        .catch(err => {
+          commit("setLoading", false);
+          console.error(err);
+        });
+    },
+    signinUser: ({ commit }, payload) => {
+      commit("clearError");
+      commit("setLoading", true);
+      apolloClient
+        .mutate({
+          mutation: SIGNIN_USER,
+          variables: payload
+        })
+        .then(({ data }) => {
+          commit("setLoading", false);
+          localStorage.setItem("token", data.signinUser.token);
+          // to make sure created method is run in main.js (we run getCurrentUser), reload the page
+          router.go();
+        })
+        .catch(err => {
+          commit("setLoading", false);
+          commit("setError", err);
+          console.error(err);
+        });
+    },
+    signoutUser: async ({ commit }) => {
+      // clear user in state
+      commit("clearUser");
+      // remove token in localStorage
+      localStorage.setItem("token", "");
+      // end session
+      await apolloClient.resetStore();
+      // redirect home - kick users out of private pages (i.e. profile)
+      router.push("/");
+    }
+  },
+  getters: {
+    user: state => state.user,
+    loading: state => state.loading,
+    error: state => state.error,
+    authError: state => state.authError
+  }
 });
